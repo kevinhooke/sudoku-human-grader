e90968c554871f93678fa27183de87e0b41a1721
kevinhooke <kevin.hooke@gmail.com>
2020-05-21 08:45:29 -0700

    difficulty score

---
+++ src/main/java/kh/sudokugrader/PuzzleDifficulty.java
@@ -0,0 +1,71 @@
+package kh.sudokugrader;
+
+public class PuzzleDifficulty {
+    
+    //simple techniques count
+    private int nakedSingleCount;
+    private int hiddenSingleCount;
+    
+    //medium techniques count
+    private int nakedPairsCount;
+    private int hiddenPairsCount;
+    
+    //hard techniques count
+    private int nakedTriplesCount;
+    private int hiddenTriplesCount;
+    private int xWingCount;
+    
+    public int incrementNakedSingleCount() {
+        this.nakedSingleCount++;
+        return this.nakedSingleCount;
+    }
+    
+    public int incrementHiddenSingleCount() {
+        this.hiddenSingleCount++;
+        return this.hiddenSingleCount;
+    }
+    
+    public int getNakedSingleCount() {
+        return nakedSingleCount;
+    }
+    public void setNakedSingleCount(int nakedSingleCount) {
+        this.nakedSingleCount = nakedSingleCount;
+    }
+    public int getHiddenSingleCount() {
+        return hiddenSingleCount;
+    }
+    public void setHiddenSingleCount(int hiddenSingleCount) {
+        this.hiddenSingleCount = hiddenSingleCount;
+    }
+    public int getNakedPairsCount() {
+        return nakedPairsCount;
+    }
+    public void setNakedPairsCount(int nakedPairsCount) {
+        this.nakedPairsCount = nakedPairsCount;
+    }
+    public int getHiddenPairsCount() {
+        return hiddenPairsCount;
+    }
+    public void setHiddenPairsCount(int hiddenPairsCount) {
+        this.hiddenPairsCount = hiddenPairsCount;
+    }
+    public int getNakedTriplesCount() {
+        return nakedTriplesCount;
+    }
+    public void setNakedTriplesCount(int nakedTriplesCount) {
+        this.nakedTriplesCount = nakedTriplesCount;
+    }
+    public int getHiddenTriplesCount() {
+        return hiddenTriplesCount;
+    }
+    public void setHiddenTriplesCount(int hiddenTriplesCount) {
+        this.hiddenTriplesCount = hiddenTriplesCount;
+    }
+    public int getxWingCount() {
+        return xWingCount;
+    }
+    public void setxWingCount(int xWingCount) {
+        this.xWingCount = xWingCount;
+    }
+    
+}
